352675afc38258b4099b90e5567ed47b8cd60412
Shailja <genieshailja@gmail.com>
2025-05-23 19:17:21 +0530

    Your commit message

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const schoolRoutes = require('./routes/schoolRoutes');
 const app = express();
 
 app.use(express.json());
-app.use('/api', schoolRoutes);
 
+app.use('/api', schoolRoutes);
+app.get('/', (req, res) => {
+  res.send('School Management API is up and running!');
+});
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);

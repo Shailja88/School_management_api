2012c38963e1802575a173e11b4fab52e6825d49
Shailja <genieshailja@gmail.com>
2025-05-23 19:34:34 +0530

    Add public folder and serve index.html on root

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -7,12 +8,17 @@ const app = express();
 
 app.use(express.json());
 
+// Serve frontend static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api', schoolRoutes);
+
+// Serve frontend index.html on root route
 app.get('/', (req, res) => {
-  res.send('School Management API is up and running!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
-

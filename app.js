e0760c4d19fac348c78a4361d72b2d8c00dc736c
Shailja <genieshailja@gmail.com>
2025-05-23 18:54:27 +0530

    Updated API logic and MySQL connection

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
